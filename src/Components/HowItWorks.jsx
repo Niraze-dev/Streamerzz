491c1c0436db23af7014f720d5d34c2a3dfc0bde
Neeraj Kumar <you@example.com>
2024-10-15 14:03:48 +0530

    commit

--- src/Components/HowItWorks.jsx
+++ src/Components/HowItWorks.jsx
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { HOW_IT_WORKS_CONTENT } from "../constants";
 import { motion } from "framer-motion";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css"; // Ensure the CSS is imported
 
 const HowItWorks = () => {
+  const [loadedImages, setLoadedImages] = useState({});
+  const [loadedProfile, setLoadedProfile] = useState({});
+
+  const handleImageLoad = (index) => {
+    setLoadedImages((prev) => ({
+      ...prev,
+      [index]: true,
+    }));
+  };
+
+  const handleProfileLoad = (index) => {
+    setLoadedProfile((prev) => ({
+      ...prev,
+      [index]: true,
+    }));
+  };
+
   const containerVariants = {
     visible: {
       transition: {
@@ -27,7 +46,10 @@ const HowItWorks = () => {
   };
 
   return (
-    <section id="HowItWorks" className="flex flex-col justify-center items-center box-border px-4 border-b border-neutral-800 lg:pb-24 md:pb-20 pb-12">
+    <section
+      id="HowItWorks"
+      className="flex flex-col justify-center items-center box-border px-4 border-b border-neutral-800 lg:pb-24 md:pb-20 pb-12"
+    >
       <motion.div
         initial={{ opacity: 0, y: -20 }}
         animate={{ opacity: 1, y: 0, transition: { duration: 1.3 } }}
@@ -40,6 +62,7 @@ const HowItWorks = () => {
           {HOW_IT_WORKS_CONTENT.sectionDescription}
         </p>
       </motion.div>
+
       <motion.div
         initial="hidden"
         whileInView="visible"
@@ -47,33 +70,40 @@ const HowItWorks = () => {
         variants={containerVariants}
         className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8"
       >
-        {HOW_IT_WORKS_CONTENT.steps.map((feature,i) => (
+        {HOW_IT_WORKS_CONTENT.steps.map((feature, i) => (
           <motion.div
-            className="flex flex-col justify-between box-border px-4 border border-neutral-800 lg:p-11 md:p-9 p-8 rounded-xl shadow-lg bg-gradient-to-b to-neutral-900  from-neutral-950"
+            className="flex flex-col justify-between box-border px-4 border border-neutral-800 lg:p-11 md:p-9 p-8 rounded-xl shadow-lg bg-gradient-to-b to-neutral-900 from-neutral-950"
             key={i}
             variants={stepVariants}
             initial="hidden"
             whileInView={"visible"}
           >
-            <h1 className="text-3xl lg:text-4xl my-4 font-semibold tracking-tighter  text-neutral-200 text-center">
+            <h1 className="text-3xl lg:text-4xl my-4 font-semibold tracking-tighter text-neutral-200 text-center">
               {feature.title}
             </h1>
             <h1 className="text-sm lg:text-xl my-4 font-semibold tracking-tighter text-neutral-500 text-center">
               {feature.description}
             </h1>
             <div className="flex justify-center">
-              <img
+              <LazyLoadImage
                 src={feature.imageSrc}
-                className="mt-8 border-neutral-800 rounded-lg text-sm border w-full overflow-hidden"
                 alt={feature.title}
+                onLoad={() => handleImageLoad(i)} // Mark image as loaded
+                className={`mt-8 border-neutral-800 rounded-lg text-sm border w-full overflow-hidden ${
+                  loadedImages[i] ? "blur-none" : "blur-lg"
+                }`} // Apply blur logic here
               />
             </div>
+
             {feature.users && (
-              <div className="flex flex-col sm:gap-0 sm:flex-row  gap-4 justify-between px-1 items-center mt-4 ">
+              <div className="flex flex-col sm:gap-0 sm:flex-row gap-4 justify-between px-1 items-center mt-4">
                 <div className="flex items-center -space-x-2">
                   {feature.users.map((user, ind) => (
                     <img
-                      className="size-11 sm:size-11 lg:size-12 rounded-full"
+                      onLoad={() => handleProfileLoad(ind)}
+                      className={`${
+                        loadedProfile[ind] ? "blur-none" : "blur-lg"
+                      } w-11 h-11 lg:w-12 lg:h-12 rounded-full`}
                       key={ind}
                       src={user}
                       alt={`User ${ind}`}
